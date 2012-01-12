75d8ff633be3add8b3848f145c4726b71bc187b0
mausch <mauricioscheffer@gmail.com>
2012-01-12 13:51:32 -0300

    started TeamCity integration

---
+++ Fuchu.TeamCity/Runner.fs
@@ -0,0 +1,36 @@
+﻿module Fuchu.TeamCity
+
+open System
+open JetBrains.TeamCity.ServiceMessages.Write
+open JetBrains.TeamCity.ServiceMessages.Write.Special
+open JetBrains.TeamCity.ServiceMessages.Write.Special.Impl
+open JetBrains.TeamCity.ServiceMessages.Write.Special.Impl.Updater
+open JetBrains.TeamCity.ServiceMessages.Write.Special.Impl.Writer
+
+let formatter = ServiceMessageFormatter()
+
+let getWriter name =
+    let msgWriter = SpecializedServiceMessagesWriter(formatter, ResizeArray([ FlowMessageUpdater() :> IServiceMessageUpdater ]), fun _ -> ())
+    new TeamCityTestWriter(msgWriter, name)
+
+let evalTeamCity (test: Test) : TestRunResult list =
+    let locker = obj()
+    let beforeRun name = 
+        let writer = getWriter name
+        lock locker (fun () -> writer.OpenTest())
+    let printPassed name time = 
+        use writer = getWriter name
+        lock locker (fun () -> writer.WriteDuration time)
+    let printFailed name error time =
+        use writer = getWriter name
+        lock locker (fun () -> 
+                        writer.WriteDuration time
+                        writer.WriteFailed(error, error))
+    let printException name (ex: #exn) time =
+        use writer = getWriter name
+        lock locker (fun () -> 
+                        writer.WriteDuration time
+                        writer.WriteFailed(ex.Message, ex.ToString()))
+    eval ignore printPassed printFailed printException pmap test
+
+let run = runEval evalTeamCity
