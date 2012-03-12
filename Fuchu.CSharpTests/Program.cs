d34a8c8dc13ef34b18ff69760c468e4945716f76
mausch <mauricioscheffer@gmail.com>
2012-03-12 01:00:52 -0300

    somewhat better C# examples, still need to add more

--- Fuchu.CSharpTests/Program.cs
+++ Fuchu.CSharpTests/Program.cs
@@ -1,27 +1,38 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using NUnit.Framework;
 
 namespace Fuchu.CSharpTests {
     internal class Program {
         public static IEnumerable<Test> Tests {
             get {
-                yield return Test.Case("Basic test", () => Assert.AreEqual(4, 2 + 2));
+                // simplest test
+                yield return 
+                    Test.Case("Basic test", () => Assert.AreEqual(4, 2 + 2));
 
+                // setup/teardown
                 var setupMemoryStream =
                     Test.Setup<MemoryStream>(setup: () => new MemoryStream(),
-                               teardown: s => {
-                                   Assert.AreEqual(5, s.Capacity);
-                                   s.Dispose();
-                               });
-                yield return Test.List("setup & teardown",
-                                          Test.Case("1", setupMemoryStream(s => s.Capacity = 5))
-                                          , Test.Case("2", setupMemoryStream(s => s.Capacity = 5)));
+                                             teardown: s => { s.Dispose(); });
+
+                yield return 
+                    Test.List("Setup & teardown with memorystream", new[] {
+                        Test.Case("Can read", setupMemoryStream(ms => {
+                            Assert.True(ms.CanRead);
+                        })),
+                        Test.Case("Can write", setupMemoryStream(ms => {
+                            Assert.True(ms.CanWrite);
+                        })),
+                    });
             }
         }
 
         private static int Main(string[] args) {
-            return Tests.Run();
+            return Tests
+                .Select(t => t.Wrap(x => x.Timeout(2500))) // set a timeout for all tests
+                .List()
+                .RunParallel(); // run all tests in parallel
         }
     }
 }
