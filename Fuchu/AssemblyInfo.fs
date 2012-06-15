ffea759045cace6ad14b5e2e715c7acae2405392
mausch <mauricioscheffer@gmail.com>
2012-06-15 18:39:18 -0300

    Assembly info

---
+++ Fuchu/AssemblyInfo.fs
@@ -0,0 +1,13 @@
+﻿namespace Fuchu
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyVersion("0.1.0.0")>]
+[<assembly: AssemblyFileVersion("0.1.0.0")>]
+[<assembly: AssemblyTitle("Fuchu")>]
+[<assembly: AssemblyDescription("Functional test library")>]
+[<assembly: AssemblyProduct("Fuchu")>]
+[<assembly: AssemblyCopyright("Copyright Mauricio Scheffer 2012")>]
+do()
