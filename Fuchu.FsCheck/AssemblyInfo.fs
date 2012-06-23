0ff39adfae96d4b46a9d6076ff711ce0a1b7ca55
mausch <mauricioscheffer@gmail.com>
2012-06-23 13:06:00 -0300

    AssemblyInfo for Fuchu.FsCheck

---
+++ Fuchu.FsCheck/AssemblyInfo.fs
@@ -0,0 +1,13 @@
+﻿namespace Fuchu
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyVersion("0.1.0.0")>]
+[<assembly: AssemblyFileVersion("0.1.0.0")>]
+[<assembly: AssemblyTitle("Fuchu.FsCheck")>]
+[<assembly: AssemblyDescription("Integrates Fuchu with FsCheck")>]
+[<assembly: AssemblyProduct("Fuchu.FsCheck")>]
+[<assembly: AssemblyCopyright("Copyright Mauricio Scheffer 2012")>]
+do()
