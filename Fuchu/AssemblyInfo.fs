4ba1045a07189eaf38ace0c75073feaa06945194
mausch <mauricioscheffer@gmail.com>
2013-04-02 16:09:21 -0300

    Bump version to 0.2.2

--- Fuchu/AssemblyInfo.fs
+++ Fuchu/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 open System.Runtime.CompilerServices
 open System.Runtime.InteropServices
 
-[<assembly: AssemblyVersion("0.2.1.0")>]
-[<assembly: AssemblyFileVersion("0.2.1.0")>]
+[<assembly: AssemblyVersion("0.2.2.0")>]
+[<assembly: AssemblyFileVersion("0.2.2.0")>]
 [<assembly: AssemblyTitle("Fuchu")>]
 [<assembly: AssemblyDescription("Functional test library")>]
 [<assembly: AssemblyProduct("Fuchu")>]
-[<assembly: AssemblyCopyright("Copyright Mauricio Scheffer 2012")>]
+[<assembly: AssemblyCopyright("Copyright Mauricio Scheffer 2013")>]
 do()
