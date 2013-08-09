ab02236bd18713e2ba8766f97fc9d034f3796d8f
mausch <mauricioscheffer@gmail.com>
2013-08-09 18:42:45 -0300

    Bump version to 0.3.0.1

--- Fuchu.FsCheck/AssemblyInfo.fs
+++ Fuchu.FsCheck/AssemblyInfo.fs
@@ -4,8 +4,8 @@ open System.Reflection
 open System.Runtime.CompilerServices
 open System.Runtime.InteropServices
 
-[<assembly: AssemblyVersion("0.3.0.0")>]
-[<assembly: AssemblyFileVersion("0.3.0.0")>]
+[<assembly: AssemblyVersion("0.3.0.1")>]
+[<assembly: AssemblyFileVersion("0.3.0.1")>]
 [<assembly: AssemblyTitle("Fuchu.FsCheck")>]
 [<assembly: AssemblyDescription("Integrates Fuchu with FsCheck")>]
 [<assembly: AssemblyProduct("Fuchu.FsCheck")>]
