55d995f01a721b228c6c347ebe04377d4c389895
mausch <mauricioscheffer@gmail.com>
2013-11-21 12:13:42 -0300

    Run tests in build script

--- build.bat
+++ build.bat
@@ -6,7 +6,7 @@ if not exist packages\FAKE\tools\Fake.exe (
 	echo Downloading FAKE...
 	".nuget\NuGet.exe" "install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion" "-Prerelease"
 )
-"packages\FAKE\tools\Fake.exe" "build.fsx"
+"packages\FAKE\tools\Fake.exe" "build.fsx" %*
 
 ::it's windows and we should expect user just click bat file, so we show result
 pause
--- build.fsx
+++ build.fsx
@@ -2,6 +2,7 @@
 #r @"FakeLib.dll"
 
 open Fake
+open Fake.ProcessHelper
 
 let buildDir = "./build"
 let nugetDir = "./.nuget"
@@ -11,13 +12,24 @@ Target "Clean" (fun _ -> CleanDirs [buildDir])
 
 Target "RestorePackages" RestorePackages
 
-Target "BuildSolution" (fun _ ->        
+Target "BuildSolution" (fun _ ->
     MSBuildWithDefaults "Build" ["./Fuchu.sln"]
     |> Log "AppBuild-Output: "
 )
 
-"Clean"
-    ==> "RestorePackages"
-    ==> "BuildSolution"
+Target "Test" <| fun _ ->
+    let errorCode = 
+        [
+            @"Fuchu.Tests\bin\Debug\Fuchu.Tests.exe"
+            @"Fuchu.CSharpTests\bin\Debug\Fuchu.CSharpTests.exe"
+        ]
+        |> Seq.map (fun p -> asyncShellExec { defaultParams with Program = p })
+        |> Async.Parallel
+        |> Async.RunSynchronously
+        |> Array.sum
+    if errorCode <> 0 then failwith "Error in tests"
+
+"BuildSolution" <== ["Clean"; "RestorePackages"]
+"Test" <== ["BuildSolution"]
 
 RunTargetOrDefault "BuildSolution"
