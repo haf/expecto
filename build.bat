53e2f9b0aede82085e54a16f3c01ed2404351f4d
Heather <heather@cynede.net>
2013-11-22 08:46:29 +0400

    It's rare when people have MSBuild in Path

--- build.bat
+++ build.bat
@@ -1,7 +1,11 @@
 @echo off
 
 cls
-if not exist .nuget\nuget.exe msbuild .nuget\nuget.targets /t:CheckPrerequisites
+if %PROCESSOR_ARCHITECTURE%==x86 (
+         set MSBUILD="%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
+) else ( set MSBUILD="%SystemRoot%\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe"
+)
+if not exist .nuget\nuget.exe %MSBUILD% .nuget\nuget.targets /t:CheckPrerequisites
 if not exist packages\FAKE\tools\Fake.exe ( 
 	echo Downloading FAKE...
 	".nuget\NuGet.exe" "install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion" "-Prerelease"
