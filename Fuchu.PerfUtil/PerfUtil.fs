bcfe0d16f9901db1a4c512fbb5e019e8ae316a83
Henrik <henrik@haf.se>
2014-04-19 14:57:00 +0200

    complete docs

--- Fuchu.PerfUtil/PerfUtil.fs
+++ Fuchu.PerfUtil/PerfUtil.fs
@@ -61,6 +61,13 @@ module FuchuPerfUtil =
             conf.handleResults results
 
     /// Compares given implementation performance against a collection of other implementations.
+    /// Use the 'perfTest' function to easily construct test cases. With this function, the configuration
+    /// will be the sane defaults; if you want to override them, please see <see cref="testPerfImplsWithConfig" />.
+    ///
+    /// <param name="name">Name for the group of performance tests</param>
+    /// <param name="subject">Implementation under test.</param>
+    /// <param name="alternatives">Secondary implementations to be compared against.</param>
+    /// <param name="tests">The performance tests to run against the subject and the alternatives.</param>
     let testPerfImpls<'a when 'a :> ITestable> name (subj : 'a) (alts : 'a list) (tests : 'a PerfTest list) =
         testPerfImplsWithConfig PerfImplsConf.Defaults name subj alts tests
 
@@ -69,10 +76,18 @@ module FuchuPerfUtil =
           /// path to history file
         { historyFile   : string
           comparer      : IPerformanceComparer
+          /// Whether to print results to stdout. Defaults to true.
           verbose       : bool
+          /// Whether to throw if the subject has gotten worse in comparison to previous runs, 
+          /// as decided by the 'comparer'.
           throwOnError  : bool
+          /// Whether to overwrite previous tests. Defaults to true.
           overwrite     : bool
-          handleResults : TestSession list -> unit }
+          /// An optional function that is called when the perf tests have been completed
+          /// allowing you to extrace the results and save them or display them or show them
+          /// to your mom. It will be passed the path of the xml file with test results and
+          /// the list of TestSessions that comes from PerfUtil.
+          handleResults : string * TestSession list -> unit }
         /// Defaults to a xml file in the currently executing DLL's directory
         /// named the same as the collection of perf tests.
         static member Defaults testName =
@@ -84,6 +99,15 @@ module FuchuPerfUtil =
               handleResults = fun _ -> () }
 
     /// Compares current implementation against a collection of past tests.
+    /// <param name="conf">Configuration for the historical performance test</param>
+    /// <param name="name">Name for the group of performance tests</param>
+    /// <param name="subject">(Current) implementation under test.</param>
+    /// <param name="testRunId">The id of the test run; this must be ticking upwards, so
+    /// a recommended value for this parameter is the current assembly version. You can use
+    /// for example https://github.com/Albacore/albacore/#docs-asmver together with
+    /// https://github.com/Albacore/albacore/#versionizer and
+    /// https://github.com/haf/semver to manage your versions in a CI-environment.</param>
+    /// <param name="tests">The performance tests to run against the subject and the alternatives.</param>
     let testPerfHistoryWithConfig (conf : PerfHistoryConf) name (subject : 'a) (testRunId : string) (tests : 'a PerfTest list) =
         let tester =
             new PastImplementationComparer<_>(
@@ -93,9 +117,17 @@ module FuchuPerfUtil =
         testCase name <| fun _ ->
             let results = PerfTest.run (fun () -> tester :> PerformanceTester<'a>) tests
             tester.PersistCurrentResults()
-            conf.handleResults results
+            conf.handleResults(conf.historyFile, results)
 
     /// Compares current implementation against a collection of past tests.
+    /// <param name="name">Name for the group of performance tests</param>
+    /// <param name="subject">(Current) implementation under test.</param>
+    /// <param name="testRunId">The id of the test run; this must be ticking upwards, so
+    /// a recommended value for this parameter is the current assembly version. You can use
+    /// for example https://github.com/Albacore/albacore/#docs-asmver together with
+    /// https://github.com/Albacore/albacore/#versionizer and
+    /// https://github.com/haf/semver to manage your versions in a CI-environment.</param>
+    /// <param name="tests">The performance tests to run against the subject and the alternatives.</param>
     let testPerfHistory name (subject : 'a) (testRunId : string) =
         testPerfHistoryWithConfig (PerfHistoryConf.Defaults name) name subject testRunId
 
