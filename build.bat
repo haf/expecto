543565148141042a8fa88b5231792a734a7b6463
mausch <mauricioscheffer@gmail.com>
2014-04-25 18:57:11 -0300

    No pause to avoid problems with build servers

--- build.bat
+++ build.bat
@@ -11,6 +11,3 @@ if not exist packages\FAKE\tools\Fake.exe (
 	".nuget\NuGet.exe" "install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion" "-Prerelease"
 )
 "packages\FAKE\tools\Fake.exe" "build.fsx" %*
-
-::it's windows and we should expect user just click bat file, so we show result
-pause
