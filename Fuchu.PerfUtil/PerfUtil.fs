9226a9530ca6ddaa8d44b5267885a8092b7fee0b
Henrik <henrik@haf.se>
2014-04-25 12:13:10 -0300

    finished PerfUtil impl, started adding charting support
    Conflicts:
    	packages/repositories.config

--- Fuchu.PerfUtil/PerfUtil.fs
+++ Fuchu.PerfUtil/PerfUtil.fs
@@ -3,16 +3,21 @@
 open System
 open global.PerfUtil
 
+module PerfUtilFac =
+    let todo () = ()
+
 [<AutoOpen; CompilationRepresentationAttribute(CompilationRepresentationFlags.ModuleSuffix)>]
 module FuchuPerfUtil =
     open Fuchu
     open Fuchu.Helpers
     open Fuchu.Impl
 
+    open System.IO
+
     type PerfConfig =
         { a : string }
 
-    type CompareConfig =
+    type CompareImplsConfig =
         { throwOnError : bool
           comparer     : IPerformanceComparer
           verbose      : bool }
@@ -21,27 +26,58 @@ module FuchuPerfUtil =
               comparer     = MeanComparer()
               verbose      = true }
 
+    /// A configuration for the historical performance development
+    /// for a given thing.
+    type CompareHistoryConfig =
+          /// path to history file
+        { historyFile  : string
+          comparer     : IPerformanceComparer
+          verbose      : bool
+          throwOnError : bool
+          overwrite    : bool }
+        /// Defaults to a xml file in the currently executing DLL's directory
+        /// named the same as the collection of perf tests.
+        static member Defaults testName =
+            { historyFile  = Path.Combine(Path.GetDirectoryName(PerfUtil.DefaultPersistenceFile), testName + ".xml")
+              comparer     = MeanComparer()
+              verbose      = true
+              throwOnError = true
+              overwrite    = true }
+
+    /// Create a new performance test. The values given from this method are usable both
+    /// by PerfUtil as well as Fuchu's testPerfCompareWithConfig, testPerfCompare,
+    /// testPerfHistoryWithConfig and testPerfHistory. You can give the values from this
+    /// function to both Fuchu and PerfUtil.
     let perfTest name (testImpl : 'a -> unit when 'a :> ITestable) =
         { PerfTest.Id = name
           Test        = testImpl }
 
-    let testPerfCompareWithConfig (conf : CompareConfig) name (subject : 'a) (alternatives : 'a list) (tests : PerfTest<'a> list) =
+    let testPerfCompareWithConfig (conf : CompareImplsConfig) name (subject : 'a) (alternatives : 'a list) (tests : PerfTest<'a> list) =
         let tester () =
             new ImplemantationComparer<_>(subject, alternatives, conf.comparer, conf.verbose, conf.throwOnError)
                 :> PerformanceTester<'a>
 
         testCase name <| fun _ ->
             let results = PerfTest.run tester tests
-            () // TODO: handle saving of results
+            // TODO: handle saving or displaying of results
+            // TODO: handle by saving artifacts? Chart them and save charts?
+            ()
 
     let testPerfCompare<'a when 'a :> ITestable> name (subj : 'a) (alts : 'a list) (tests : 'a PerfTest list) =
-        testPerfCompareWithConfig CompareConfig.Defaults name subj alts tests
+        testPerfCompareWithConfig CompareImplsConfig.Defaults name subj alts tests
 
-    let testPerfHistoryWithConfig config name =
-        ()
+    let testPerfHistoryWithConfig (config : CompareHistoryConfig) name (subject : 'a) (testRunId : string) (tests : 'a PerfTest list) =
+        let tester =
+            new PastImplementationComparer<_>(
+                subject, testRunId, config.historyFile, config.comparer,
+                config.verbose, config.throwOnError, config.overwrite)
 
-    let testPerfHistory name =
-        ()
+        testCase name <| fun _ ->
+            let results = PerfTest.run (fun () -> tester :> PerformanceTester<'a>) tests
+            tester.PersistCurrentResults()
+
+    let testPerfHistory name (subject : 'a) (testRunId : string) =
+        testPerfHistoryWithConfig (CompareHistoryConfig.Defaults name) name subject testRunId
 
 module Usage =
     open global.PerfUtil
@@ -54,7 +90,7 @@ module Usage =
         inherit ITestable
         abstract member Serialise<'a> : 'a -> unit
 
-    type SlowSerialiser() =
+    type MySlowSerialiser() =
         interface ITestable with
             member x.Name = "Slow Serialiser"
         interface Serialiser with
@@ -76,19 +112,23 @@ module Usage =
                 System.Threading.Thread.Sleep(20)
 
     let alts : Serialiser list = [ FastSerialiser(); FastSerialiserAlt() ]
-    let subj = SlowSerialiser() :> Serialiser
+    let subj = MySlowSerialiser() :> Serialiser
 
     open Types
 
+    let normal_serlialisation : PerfTest<Serialiser> list = [
+        perfTest "serialising string" <| fun s ->
+            s.Serialise("wowowow")
+        perfTest "serialising record" <| fun s ->
+            s.Serialise { a = "hello world"; b = 42 }
+        ]
+
+    [<Tests>]
     let tests =
-        testList "performance tests" [
-            testPerfCompare "implementations of Serialiser" subj alts [
-                perfTest "serialising string" <| fun s ->
-                    s.Serialise("wowowow")
-                perfTest "serialising record" <| fun s ->
-                    s.Serialise { a = "hello world"; b = 42 }
-                ]
-            ]
+        testList "performance comparison tests" [
+            testPerfCompare "implementations of Serialiser" subj alts normal_serlialisation
+            testPerfHistory "historical MySlowSerialiser" subj "v1.2.3" normal_serlialisation
+        ]
 
 (* EXAMPLES:
 
