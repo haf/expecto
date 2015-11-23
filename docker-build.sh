f81e3eaa54fa275c35790a9717a3077ffceb546e
Mauricio Scheffer <mauricioscheffer@gmail.com>
2015-11-23 00:41:37 +0000

    mono 4.2

--- docker-build.sh
+++ docker-build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -v $PWD:/opt/fuchu -w /opt/fuchu mono:4 bash -c 'apt-get install ca-certificates && ./build.sh'
+docker run --rm -v $PWD:/opt/fuchu -w /opt/fuchu mono:4.2 bash -c 'apt-get install ca-certificates && ./build.sh'
