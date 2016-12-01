595e18c10835abb57f9a440486b80dd0a270ab2f
Jared Hester <jared.hester@gmail.com>
2016-11-30 21:07:24 -0500

    setup FAKE build for netframework and netcore

--- build.fsx
+++ build.fsx
@@ -1,18 +1,66 @@
 #I @"packages/build/FAKE/tools/"
 #r @"FakeLib.dll"
-
+System.IO.Directory.SetCurrentDirectory __SOURCE_DIRECTORY__
+open System
 open Fake
 
-let solutionFile = "Expecto"
+
+
+let run cmd args dir =
+  if execProcess( fun info ->
+    info.FileName <- cmd
+    if not( String.IsNullOrWhiteSpace dir) then
+      info.WorkingDirectory <- dir
+    info.Arguments <- args
+  ) System.TimeSpan.MaxValue = false then
+      failwithf "Error while running '%s' with args: %s" cmd args
+
+
+// --------------------------------------------------------------------------------------
+// Clean Build Detritus
+
+Target "Clean" (fun _ ->
+  !! "/**/bin/"
+  ++ "/**/obj/"
+  |> CleanDirs
+  |> ignore
+)
+
 
 // --------------------------------------------------------------------------------------
 // Build library, test project, & sample
 
+let solutionFile = "Expecto"
+
 Target "Build" (fun _ ->
-    // We would like to build only one solution
-    !! (solutionFile + ".sln")
-    |> MSBuildRelease "" "Rebuild"
-    |> ignore
+  !! (solutionFile + ".sln")
+  |> MSBuildRelease "" "Rebuild"
+  |> ignore
 )
 
-RunTargetOrDefault "Build"
+
+// --------------------------------------------------------------------------------------
+// Build netcore expecto library
+
+let netcoreDir = "Expecto.netcore"
+let dotnet args dir = run "dotnet" args dir
+
+Target "DotnetBuild" (fun _ ->
+  dotnet "restore" netcoreDir
+  dotnet "build"   netcoreDir
+)
+
+
+// --------------------------------------------------------------------------------------
+// Target Dependencies
+
+Target "All" DoNothing
+
+"Clean"
+  ==> "Build"
+  <=> "DotnetBuild"
+
+"Build"       ==> "All"
+"DotnetBuild" ==> "All"
+
+RunTargetOrDefault "All"
