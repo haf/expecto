cda5f060e06d52910e8ce0febfb469fd9146a91c
Henrik Feldt <henrik@haf.se>
2017-01-17 21:40:39 +0100

    Whitespace

--- Expecto.FsCheck/FsCheck.fs
+++ Expecto.FsCheck/FsCheck.fs
@@ -9,15 +9,15 @@ module ExpectoFsCheck =
 
   let private propertyTest methodName focusState config name property =
 
-    /// This running plugs into FsCheck and gives us the ability to
-    /// get called by the framework on start/finish of fixtues.
+    /// This running plugs into FsCheck and gives us the ability to get called
+    /// by the framework on start/finish of fixtures.
     let runner =
       { new IRunner with
           member __.OnStartFixture _ = ()
           member __.OnArguments(_,_,_) = ()
           member __.OnShrink(_,_) = ()
-          member __.OnFinished(_,testResult) =
-            let numTests i = if i=1 then "1 test" else string i+" tests"
+          member __.OnFinished(_, testResult) =
+            let numTests i = if i = 1 then "1 test" else sprintf "%i tests" i
             match testResult with
             | TestResult.True (_testData,_b) ->
               ()
@@ -30,7 +30,7 @@ module ExpectoFsCheck =
               sprintf
                 "Failed after %s. Parameters: %s Result: %A\n%s (%i,%i)"
                 (numTests data.NumberOfTests)
-                (String.Join(" ",List.map (sprintf "%A") original) +
+                (String.Join(" ", List.map (sprintf "%A") original) +
                   if data.NumberOfShrinks>0 then
                     " (Shrunk: " +
                         String.Join(" ",List.map (sprintf "%A") shrunk) + ")"
@@ -57,23 +57,28 @@ module ExpectoFsCheck =
   /// Builds a test property with config
   let testPropertyWithConfig config name =
     propertyTest "ftestPropertyWithConfig" Normal config name
-  /// Builds a test property with config that will be ignored by Expecto
+
+  /// Builds a test property with config that will be ignored by Expecto.
   let ptestPropertyWithConfig config name =
     propertyTest "ftestPropertyWithConfig" Pending config name
+
   /// Builds a test property with config that will make Expecto to
-  /// ignore other unfocused tests
+  /// ignore other unfocused tests.
   let ftestPropertyWithConfig stdGen config name =
     let config =
       { config with
           Replay = Random.StdGen stdGen |> Some }
     propertyTest "ftestPropertyWithConfig" Focused config name
-  /// Builds a test property
+
+  /// Builds a test property.
   let testProperty name =
     propertyTest "ftestProperty" Normal Config.Default name
-  /// Builds a test property that will be ignored by Expecto
+
+  /// Builds a test property that will be ignored by Expecto.
   let ptestProperty name =
     propertyTest "ftestProperty" Pending Config.Default name
-  /// Builds a test property that will make Expecto to ignore other unfocused tests
+
+  /// Builds a test property that will make Expecto to ignore other unfocused tests.
   let ftestProperty stdGen name =
     let config =
       { Config.Default with
