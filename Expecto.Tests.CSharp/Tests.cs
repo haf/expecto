6be9e6a26d27b656ec9a2e4cc15133f060096868
Chet Husk <chusk3@gmail.com>
2017-02-22 22:34:12 -0600

    don't ?

--- Expecto.Tests.CSharp/Tests.cs
+++ Expecto.Tests.CSharp/Tests.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
-using static Expecto.CSharp.Runner;
+using Expecto.CSharp;
 using Expecto;
 
 namespace Test.CSharp
@@ -9,24 +9,24 @@ namespace Test.CSharp
     {
         [Tests]
         public static Expecto.Test blah =
-            TestList("general groupings", new Expecto.Test[] {
-                TestCase("standard", () => Console.Write("standard")),
-                TestCase("standard async", async () => {
+            Runner.TestList("general groupings", new Expecto.Test[] {
+                Runner.TestCase("standard", () => Console.Write("standard")),
+                Runner.TestCase("standard async", async () => {
                     await Task.Delay(100);
                     Console.Write("standard task");
                 }),
-                PendingTestCase("pending", () => Console.Write("pending")),
-                PendingTestCase("pending async", async () => {
+                Runner.PendingTestCase("pending", () => Console.Write("pending")),
+                Runner.PendingTestCase("pending async", async () => {
                     await Task.Delay(100);
                     Console.Write("standard task");
                 }),
-                FocusedTestCase("focused", () => Console.Write("focused")),
-                FocusedTestCase("focused async", async () => {
+                Runner.FocusedTestCase("focused", () => Console.Write("focused")),
+                Runner.FocusedTestCase("focused async", async () => {
                     await Task.Delay(100);
                     Console.Write("standard task");
                 }),
             });
 
-		public static int Main(string[] argv) => RunTestsInAssembly(DefaultConfig, argv);
+		public static int Main(string[] argv) => Runner.RunTestsInAssembly(Runner.DefaultConfig, argv);
     }
 }
