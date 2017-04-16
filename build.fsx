71d03ad2df8d31eb41242cebcd562af2d593c274
Adam Chester <adamchester@gmail.com>
2017-04-16 14:23:47 +1000

    Update windows, travis and appveyor builds

--- build.fsx
+++ build.fsx
@@ -18,6 +18,45 @@ let run cmd args dir =
   ) System.TimeSpan.MaxValue = false then
       failwithf "Error while running '%s' with args: %s" cmd args
 
+
+let project = "Expecto"
+let summary = "A smooth unit test framework for F#"
+let description = summary
+
+// List of author names (for NuGet package)
+let authors = [ "Logibit AB" ]
+let copyright = "(c) 2016 by Henrik Feldt, formerly Fuchu by @mausch"
+// Tags for your project (for NuGet package)
+
+let version = { Major = 5; Minor = 0; Patch = 0 }
+
+open Fake.AssemblyInfoFile
+
+let genFSAssemblyInfo (projectPath) =
+    let projectName = System.IO.Path.GetFileNameWithoutExtension(projectPath)
+    let folderName = System.IO.Path.GetFileName(System.IO.Path.GetDirectoryName(projectPath))
+    let basePath = folderName
+    let fileName = basePath @@ "AssemblyVersionInfo.fs"
+    CreateFSharpAssemblyInfo fileName [ 
+        Attribute.Title (projectName)
+        Attribute.Product project
+        Attribute.Copyright copyright
+        Attribute.Company (authors |> String.concat ", ")
+        Attribute.Description summary
+        Attribute.Version <| string version
+        // Attribute.Version release.AssemblyVersion
+        // Attribute.FileVersion release.AssemblyVersion
+        // Attribute.InformationalVersion release.NugetVersion 
+    ]
+
+
+// Generate assembly info files with the right version & up-to-date information
+Target "AssemblyInfo" (fun _ ->
+    let fsProjs =  !! "./**/*.fsproj" |> Seq.filter (fun s -> not <| s.Contains "preview")
+    fsProjs |> Seq.iter genFSAssemblyInfo
+)
+
+
 // --------------------------------------------------------------------------------------
 // Rename Logary Facades for Expecto
 //  (why this is necessary - https://github.com/logary/logary/#using-logary-in-a-library)
@@ -49,10 +88,17 @@ Target "Clean" (fun _ ->
 // Build library, test project, & sample
 
 let solutionFile = "Expecto"
+MSBuildDefaults <- { MSBuildDefaults with Verbosity = Some Minimal }
 
 Target "Build" (fun _ ->
   !! (solutionFile + ".sln")
-  |> MSBuildRelease "" "Rebuild"
+  |> MSBuildWithDefaults "Rebuild"
+  |> ignore
+)
+
+Target "BuildFast" (fun _ ->
+  !! (solutionFile + ".sln")
+  |> MSBuildWithDefaults "Build"
   |> ignore
 )
 
@@ -66,12 +112,19 @@ Target "RunTests" (fun _ ->
 // Build netcore expecto library
 
 let netcoreDir = "Expecto.netcore"
+let netcoreTestsDir = "Expecto.netcore.Tests"
 let dotnet args dir = run "dotnet" args dir
 
 Target "DotnetBuild" (fun _ ->
   dotnet "--info"  ""
   dotnet "restore" netcoreDir
   dotnet "build"   netcoreDir
+)
+
+Target "DotnetRunTests" (fun _ ->
+  dotnet "--info"  ""
+  dotnet "restore" netcoreTestsDir
+  dotnet "run -c Release --parallel --fail-on-focused-tests --summary --version" netcoreTestsDir
 )
 
 Target "CreateNuGets" (fun _ ->
@@ -88,13 +141,16 @@ Target "CreateNuGets" (fun _ ->
 
 Target "All" DoNothing
 
-"ExpectoChangeo"
+
+"AssemblyInfo"
+  ==> "ExpectoChangeo"
   ==> "Clean"
   ==> "Build"
+  ==> "DotnetRunTests"
   ==> "RunTests"
   <=> "DotnetBuild"
 
 "Build"       ==> "All"
 "DotnetBuild" ==> "All"
 
-RunTargetOrDefault "All"
+RunTargetOrDefault "DotnetBuild"
