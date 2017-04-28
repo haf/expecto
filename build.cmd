0b13d7b92abe8d73d01533ca0f893c06065c77c6
Henrik Feldt <henrik@haf.se>
2017-04-28 11:41:36 +0200

    Use rake

--- build.cmd
+++ build.cmd
@@ -1,14 +1,4 @@
 @echo off
 
-.paket\paket.bootstrapper.exe 4.4.0
-if errorlevel 1 (
-  exit /b %errorlevel%
-)
-
-
-.paket\paket.exe restore
-if errorlevel 1 (
-  exit /b %errorlevel%
-)
-
-packages\build\FAKE\tools\FAKE.exe build.fsx %*
+bundle
+bundle exec rake %*
