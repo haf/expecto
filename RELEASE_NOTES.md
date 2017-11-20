7f42bba3f14fb215dd9f326bdee51609a8881239
Anthony Lloyd <anthony@lloydhq.com>
2017-11-20 20:26:26 +0000

    FAKE

---
+++ RELEASE_NOTES.md
@@ -0,0 +1,3 @@
+#### 5.0.2 - 2017-11-20
+* Put some notes - https://github.com/fsharp/FAKE/pull/1443
+* Hello - https://github.com/fsharp/FAKE/pull/1441
--- build.fsx
+++ build.fsx
@@ -3,6 +3,31 @@
 open System
 open Fake
 
-Target "Test" ignore
+Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+let release = IO.File.ReadAllLines "RELEASE_NOTES.md" |> ReleaseNotesHelper.parseReleaseNotes
 
-RunTargetOrDefault "Test"
+open AssemblyInfoFile
+Target "AssemblyInfo" (fun _ ->
+
+    let create product =
+        [ 
+            Attribute.Product product
+            Attribute.Copyright "\169 Anthony Lloyd (formerly Henrik Feldt and cloned from Fuchu by @mausch)"
+            Attribute.Description "A smooth unit test framework for F#"
+            Attribute.Version release.AssemblyVersion
+            Attribute.FileVersion release.AssemblyVersion
+        ] |> CreateFSharpAssemblyInfo ("src/"+product+"/AssemblyInfo.fs")
+
+    create "Expecto"
+    create "Expecto.BenchmarkDotNet"
+    create "Expecto.FsCheck"
+)
+
+Target "PaketFiles" (fun _ ->
+    FileHelper.ReplaceInFiles ["namespace Logary.Facade","namespace Expecto.Logging"]
+        ["paket-files/logary/logary/src/Logary.Facade/Facade.fs"]
+)
+
+"AssemblyInfo" ==> "PaketFiles"
+
+RunTargetOrDefault "PaketFiles"
