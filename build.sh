36926622a1599d7aeacef04a12e2ed922f02a675
Anthony Lloyd <anthony.lloyd@alphakinetic.com>
2018-01-03 10:42:20 +0000

    move override

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+export FrameworkPathOverride=$(dirname $(which mono))/../lib/mono/4.5/
 mono .paket/paket.exe restore
 mono packages/build/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx
