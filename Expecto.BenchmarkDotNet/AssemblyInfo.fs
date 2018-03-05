e8a00f37389048e07b9e0bdd725c289eff6223e1
Anthony Lloyd <anthony@lloydhq.com>
2018-03-05 21:14:28 +0000

    update for 6.0.0

--- Expecto.BenchmarkDotNet/AssemblyInfo.fs
+++ Expecto.BenchmarkDotNet/AssemblyInfo.fs
@@ -6,8 +6,8 @@ open System.Reflection
 [<assembly: AssemblyProductAttribute("Expecto.BenchmarkDotNet")>]
 [<assembly: AssemblyCopyrightAttribute("Copyright © 2018")>]
 [<assembly: AssemblyDescriptionAttribute("Advanced testing library for F#")>]
-[<assembly: AssemblyVersionAttribute("5.1.2")>]
-[<assembly: AssemblyFileVersionAttribute("5.1.2")>]
+[<assembly: AssemblyVersionAttribute("6.0.0")>]
+[<assembly: AssemblyFileVersionAttribute("6.0.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
@@ -15,5 +15,5 @@ module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyProduct = "Expecto.BenchmarkDotNet"
     let [<Literal>] AssemblyCopyright = "Copyright © 2018"
     let [<Literal>] AssemblyDescription = "Advanced testing library for F#"
-    let [<Literal>] AssemblyVersion = "5.1.2"
-    let [<Literal>] AssemblyFileVersion = "5.1.2"
+    let [<Literal>] AssemblyVersion = "6.0.0"
+    let [<Literal>] AssemblyFileVersion = "6.0.0"
