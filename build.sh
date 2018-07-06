dfa536e7f9cd3d29c7d6bb9e7bb58bac294ebe5e
Anthony Lloyd <anthony@lloydhq.com>
2018-07-06 12:42:13 +0100

    upgrade fake

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export FrameworkPathOverride=$(dirname $(which mono))/../lib/mono/4.6.1-api
-mono .paket/paket.exe restore
-mono packages/build/FAKE/tools/FAKE.exe $@ --removeLegacyFakeWarning --fsiargs -d:MONO build.fsx
--- docker-build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run --rm -v $PWD:/opt/expecto -w /opt/expecto fsharp:latest bash -c './build.sh'
