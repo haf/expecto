522bd510d98fd2cd953955e11285ed688d210322
Henrik <henrik@haf.se>
2018-11-17 14:00:51 +0100

    Bump version to 8.6.0

--- Expecto.BenchmarkDotNet/AssemblyInfo.fs
+++ Expecto.BenchmarkDotNet/AssemblyInfo.fs
@@ -6,8 +6,8 @@ open System.Reflection
 [<assembly: AssemblyProductAttribute("Expecto.BenchmarkDotNet")>]
 [<assembly: AssemblyCopyrightAttribute("Copyright 2018")>]
 [<assembly: AssemblyDescriptionAttribute("Advanced testing library for F#")>]
-[<assembly: AssemblyVersionAttribute("8.5.0")>]
-[<assembly: AssemblyFileVersionAttribute("8.5.0")>]
+[<assembly: AssemblyVersionAttribute("8.6.0")>]
+[<assembly: AssemblyFileVersionAttribute("8.6.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
@@ -15,5 +15,5 @@ module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyProduct = "Expecto.BenchmarkDotNet"
     let [<Literal>] AssemblyCopyright = "Copyright 2018"
     let [<Literal>] AssemblyDescription = "Advanced testing library for F#"
-    let [<Literal>] AssemblyVersion = "8.5.0"
-    let [<Literal>] AssemblyFileVersion = "8.5.0"
+    let [<Literal>] AssemblyVersion = "8.6.0"
+    let [<Literal>] AssemblyFileVersion = "8.6.0"
