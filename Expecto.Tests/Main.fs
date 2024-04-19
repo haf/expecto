dd722479a121dcfa2ee7477a7f7d0592dad43aa4
Jimmy Byrd <jimmy.byrd@motivity.net>
2024-04-18 22:54:29 -0400

    cleanup

--- Expecto.Tests/Main.fs
+++ Expecto.Tests/Main.fs
@@ -7,38 +7,21 @@ open OpenTelemetry
 open OpenTelemetry.Trace
 open System.Threading
 open System.Diagnostics
+open System
 
 let serviceName = "Expecto.Tests"
 
 let logger = Log.create serviceName
 
 
-let resourceBuilder =
-    ResourceBuilder
-        .CreateDefault()
-        .AddService(serviceName = serviceName)
-
-
-
 [<EntryPoint>]
 let main args =
-  let activitySource = new ActivitySource(serviceName)
-  use traceProvider =
-          Sdk
-              .CreateTracerProviderBuilder()
-              .AddSource(serviceName)
-              .SetResourceBuilder(resourceBuilder )
-              .AddOtlpExporter()
-              .Build()
-  let tracer = traceProvider.GetTracer(serviceName)
-  // use span = tracer.StartActiveSpan("Expecto.main")
-  use span = tracer.StartRootSpan("Expecto.main")
+
   let test =
     Impl.testFromThisAssembly()
     |> Option.orDefault (TestList ([], Normal))
-    |> OpenTelemetry.addOpenTelemetry_SpanPerTest Impl.ExpectoConfig.defaultConfig activitySource
     |> Test.shuffle "."
-  runTestsWithCLIArgs [NUnit_Summary "bin/Expecto.Tests.TestResults.xml"] args test
+  runTestsWithCLIArgs [NUnit_Summary "bin/Expecto.Tests.TestResults.xml";] args test
 
 
 
